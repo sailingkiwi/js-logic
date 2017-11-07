2ca41094ee5ae6f6044c146481c062787f51b4a0
Ritsu <sailingkiwi@gmail.com>
2017-11-06 21:56:29 -1000

    daClub

--- exercises.js
+++ exercises.js
@@ -245,14 +245,14 @@ console.log(evenStevens(25));
  * Console.log your result.
 */ 
 function daClub(cover, age){
-	if(cover > 21, age >21){
+	if(cover > 21 && age >21){
 		return "Welcome to the Legends Lounge." ;
 	}else{
 		return "Chuck E Cheese is across the street.";
 	}
 }
  console.log(daClub(50, 41));
- 
+
 /*
  * #12
  * Function - graduation
