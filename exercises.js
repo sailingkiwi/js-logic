9795d648f041a1ec2a1736dd98217839b040887c
Ritsu <sailingkiwi@gmail.com>
2017-11-07 22:12:28 -1000

    allStars

--- exercises.js
+++ exercises.js
@@ -440,6 +440,23 @@ console.log("sumItUp ", sumItUp(numArray));
 */ 
 
 var players = ["Yao Ming", "BrookLin", " Jesus Shuttlesworth", "Manute Bol", "Sidney Deane", "World B Free"];
+var players = ["Yao Ming", "BrookLin", " Jesus Shuttlesworth", "Manute Bol", "Sidney Deane", "World B Free"];
+  var east = [];
+  var west = [];
+
+  function allStars(ballers){
+    for(var i = 0; i<ballers.length; i++){
+      console.log(ballers[i]);
+      if(i%2 === 0){
+        east.push(ballers[i]);
+      }else{
+        west.push(ballers[i]);
+      }
+    }
+  }
+  allStars(players);
+  console.log("east: ", east);
+  console.log("west: ", west);
 /*
  * #19
  * Function - subways
